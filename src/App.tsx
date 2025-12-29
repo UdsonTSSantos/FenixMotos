235c18ec8cd3f1d85d1cc77216eea8f76fe445d5
Skip <skip@usecurling.com>
2025-12-29 18:28:32 +0000

    feat: Implement motorcycle financing system structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Toaster } from '@/components/ui/toaster'
 import { Toaster as Sonner } from '@/components/ui/sonner'
 import { TooltipProvider } from '@/components/ui/tooltip'
-import Layout from './components/Layout'
-import Index from './pages/Index'
-import NotFound from './pages/NotFound'
+import Layout from '@/components/Layout'
+import Index from '@/pages/Index'
+import NotFound from '@/pages/NotFound'
 import { DataProvider } from '@/context/DataContext'
+import { ThemeProvider } from '@/components/theme-provider'
 import Motos from '@/pages/motos/Motos'
 import MotoForm from '@/pages/motos/MotoForm'
 import Clientes from '@/pages/clientes/Clientes'
@@ -18,36 +19,38 @@ const App = () => (
   <BrowserRouter
     future={{ v7_startTransition: false, v7_relativeSplatPath: false }}
   >
-    <TooltipProvider>
-      <DataProvider>
-        <Toaster />
-        <Sonner />
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Index />} />
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <TooltipProvider>
+        <DataProvider>
+          <Toaster />
+          <Sonner />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Index />} />
 
-            <Route path="/motos" element={<Motos />} />
-            <Route path="/motos/nova" element={<MotoForm />} />
-            <Route path="/motos/:id" element={<MotoForm />} />
+              <Route path="/motos" element={<Motos />} />
+              <Route path="/motos/nova" element={<MotoForm />} />
+              <Route path="/motos/:id" element={<MotoForm />} />
 
-            <Route path="/clientes" element={<Clientes />} />
-            <Route path="/clientes/novo" element={<ClienteForm />} />
-            <Route path="/clientes/:id" element={<ClienteForm />} />
+              <Route path="/clientes" element={<Clientes />} />
+              <Route path="/clientes/novo" element={<ClienteForm />} />
+              <Route path="/clientes/:id" element={<ClienteForm />} />
 
-            <Route path="/financiamentos" element={<Financiamentos />} />
-            <Route
-              path="/financiamentos/novo"
-              element={<FinanciamentoForm />}
-            />
-            <Route
-              path="/financiamentos/:id"
-              element={<FinanciamentoDetails />}
-            />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </DataProvider>
-    </TooltipProvider>
+              <Route path="/financiamentos" element={<Financiamentos />} />
+              <Route
+                path="/financiamentos/novo"
+                element={<FinanciamentoForm />}
+              />
+              <Route
+                path="/financiamentos/:id"
+                element={<FinanciamentoDetails />}
+              />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </DataProvider>
+      </TooltipProvider>
+    </ThemeProvider>
   </BrowserRouter>
 )
 
